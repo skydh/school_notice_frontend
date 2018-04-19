e6b4922b3a1b72273b9f91556395bb8ad7f2f2a9
dh <757486169@qq.com>
2018-04-19 20:19:35 +0800

    dh

--- app/components/AllData.jsx
+++ app/components/AllData.jsx
@@ -6,59 +6,69 @@ import {login} from "../fetch/data"
 var that;
 import {hashHistory} from 'react-router'
 import {getData, postData} from '../fetch/test.js'
-require('antd/dist/antd.css') ;
+require('antd/dist/antd.css');
+import {get} from '../fetch/get.js'
 /**
  * 这里加上redux来判断是否登录状态，但是考虑学习时间成本，无奈去掉
  */
 class AllDataForm extends React.Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            isShow: true
+        }
+    }
 
     handleSubmit(e) {
 
         e.preventDefault();
         that.props.form.validateFields((err, values) => {
+            debugger
             if (!err) {
+                debugger
                 let username = that.props.form.getFieldValue("userName");
-                let password = that.props.form.getFieldValue("password");
-                let url = "user/login?username=" + username + "&password=" + password
-                login(url)
+                var cangshu = ""
+                for (var i = 0; i < username.length; i++) {
+                    cangshu = cangshu + "codes=" + username[i];
+                    if(i!=username.length-1)
+                    {
+                        cangshu=cangshu+"&"
+                    }
+
+                }
+                var result = get("http://localhost:8090/school-notice/user/allData?" + cangshu)
+                result.then(res => {
+                    return res.text()
+                }).then(text => {
+                    debugger
+                    let temp = JSON.parse(text);
+
+                })
             }
         });
     }
 
-    registerfun() {
-        hashHistory.push('/register');
-    }
 
     componentDidMount() {
         that = this;
+        var result = get("http://localhost:8090/school-notice/user/getTree")
+
+        result.then(res => {
+
+            return res.text()
+        }).then(text => {
+            debugger
+            let temp = JSON.parse(text);
+
+            this.setState({data: temp.backData});
+
+
+        })
     }
 
     render() {
-        const options = [{
-            value: 'zhejiang',
-            label: 'Zhejiang',
-            children: [{
-                value: 'hangzhou',
-                label: 'Hangzhou',
-                children: [{
-                    value: 'xihu',
-                    label: 'West Lake',
-                }],
-            }],
-        }, {
-            value: 'jiangsu',
-            label: 'Jiangsu',
-            children: [{
-                value: 'nanjing',
-                label: 'Nanjing',
-                children: [{
-                    value: 'zhonghuamen',
-                    label: 'Zhong Hua Men',
-                }],
-            }],
-        }];
-
+        
         function onChange(value) {
             console.log(value);
         }
@@ -70,17 +80,14 @@ class AllDataForm extends React.Component {
                     {getFieldDecorator('userName', {
                         rules: [{required: true, message: '请输入用户名!'}],
                     })(
-                        <Cascader options={options} onChange={onChange} placeholder="Please select"/>
+                        <Cascader options={this.state.data} onChange={onChange} placeholder="Please select"
+                                  changeOnSelect/>
                     )}
                 </FormItem>
-
-
                 <FormItem>
-
                     <Button type="primary" htmlType="submit">
                         提交信息
                     </Button>
-
                 </FormItem>
             </Form>
 
